a28f8b499ee36293b8317f6c1e3d22bac82ba4b2
Gabriel Ribeiro <Gabrielribeiro.dev23@gmail.com>
2024-02-12 18:49:33 -0300

    Versão 2.0 com muitas novidades!

--- app.js
+++ app.js
@@ -1,29 +1,88 @@
+// Número limite
+let numeroLimite = 100;
+
+// Números sorteados
+let numerosSorteados = [];
+
 // Mensagem de boas vindas
-alert('Olá! Seja bem-vindo(A) ao jogo do Número Secreto!');
-//Declaração de variáveis
-let numeroSecreto = parseInt(Math.random() *100 + 1);
-let numeroChute;
-let nuneroMaximo = 100;
-let tentativas =1;
+function mensagemDeBoasVindas(tag, texto) {
+    let mensagemDeBoasVindas = document.querySelector(tag);
+    mensagemDeBoasVindas.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2} );
 
-// Enquanto o número chutado não for igual o número secreto.
-while (numeroChute!=numeroSecreto) {
-    // Chamando o prompt.
-    numeroChute = Number(prompt(`Escolha um número entre 1 e ${nuneroMaximo}`));
-// Comparando variáveis.
+}
+
+
+
+// Função dos números randômicos
+function numeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * 100 + 1);
+    let quantidadeElementosNaLista = numerosSorteados.length;
+
+    if (quantidadeElementosNaLista === numeroLimite) {
+        numerosSorteados = [];
+    }
+
+    if (numerosSorteados.includes(numeroEscolhido)) {
+        return numeroAleatorio();
+    } else {
+        numerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+
+// Chamando a função de mensagem inicial.
+mensagemInicial();
+
+// Gerar número aleatório
+let numeroSecreto = numeroAleatorio();
+
+// Tentativas
+let tentativas = 1;
+
+// Função verificar chute
+function verificarChute() {
+    let numeroChute = document.querySelector('input').value;
+
+    // Comparando números
 if (numeroChute<1||numeroChute>100) {
-    alert('O número digitado é inválido. Tente novamente.');
-} else if (numeroChute==numeroSecreto){
-    break;
-} else if (numeroChute>numeroSecreto) {
-    alert(`O número secreto é menor do que o número ${numeroChute}`);
-} else {
-    alert(`O número secreto é maior do que o número ${numeroChute}`);
+mensagemDeBoasVindas('p', 'O número digitado é inválido');
 }
-// encrementando o número de tentativas.
-tentativas++;
-}
-// Vitória!
-let palavraTentativa = tentativas > 1 ? 'tentativas.' : 'tentativa.';
+    else if (numeroChute == numeroSecreto) {
+        mensagemDeBoasVindas('h1', 'Parabéns!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        mensagemDeBoasVindas('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else if (numeroChute > numeroSecreto) {
+        mensagemDeBoasVindas('h1', 'Você errou!');
+        mensagemDeBoasVindas('p', 'O número secreto é menor!');
+    } else {
+        mensagemDeBoasVindas('h1', 'Você errou!');
+        mensagemDeBoasVindas('p', 'O número secreto é maior!');
+    }
 
-alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+    tentativas++;
+    limparCampo();
+}
+
+// Limpar campo
+function limparCampo() {
+    let numeroChute = document.querySelector('input');
+    numeroChute.value = '';
+}
+
+// Mensagem inicial
+function mensagemInicial() {
+    mensagemDeBoasVindas('h1', 'Seja bem-vindo(a) ao jogo do Número Secreto!');
+    mensagemDeBoasVindas('p', 'Escolha um número entre 1 e 100');
+}
+
+// Novo jogo
+function reiniciarJogo() {
+    numeroSecreto = numeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
